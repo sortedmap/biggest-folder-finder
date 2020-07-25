b3332f8bd9997b22d2cf3905749a1aaa5506e303
sortedmap <sortedmap@gmail.com>
2020-07-25 15:04:12 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println(getSizeFromHumanReadable("235K"));
+        System.exit(0);
+
         String folderPath = "/Users/sortedmap/Desktop";
         File file = new File(folderPath);
 
@@ -15,8 +19,6 @@ public class Main
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(calculator);
         System.out.println(size);
-
-        //System.out.println(getFolderSize(file));
 
         long duration = System.currentTimeMillis() - start;
         System.out.println(duration + " ms");
@@ -36,7 +38,7 @@ public class Main
     }
 
     //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
-    public String getHumanReadableSize(long size)
+    public static String getHumanReadableSize(long size)
     {
         return "";
     }
@@ -44,9 +46,30 @@ public class Main
     //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
     //  24B, 234K, 36M, 34G, 42T
     //  235K => 240640
-    public long getSizeFromHumanReadable(String size)
+    public static long getSizeFromHumanReadable(String size)
     {
+        HashMap<Character, Integer> char2multiplier = getMultipliers();
+        char sizeFactor = size
+            .replaceAll("[0-9\\s+]+", "")
+            .charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        long length = multiplier * Long.valueOf(
+            size.replaceAll("[^0-9]", "")
+        );
+        return length;
+    }
 
-        return 0;
+    private static HashMap<Character, Integer> getMultipliers()
+    {
+        char[] multipliers = {'B', 'K', 'M', 'G', 'T'};
+        HashMap<Character, Integer> char2multiplier = new HashMap<>();
+        for(int i = 0; i < multipliers.length; i++)
+        {
+            char2multiplier.put(
+                multipliers[i],
+                (int) Math.pow(1024, i)
+            );
+        }
+        return char2multiplier;
     }
 }
